25b639f2daf7041c71f01efd5ad955da49bd16d0
spacepotato <rubywindowofficial1@gmail.com>
2025-08-09 07:27:30 -0400

    My disdain towards linux grows by the day

--- src/main/java/com/sp/mixin/materialsampler/linux/LinuxFixMixin.java
+++ src/main/java/com/sp/mixin/materialsampler/linux/LinuxFixMixin.java
@@ -10,18 +10,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 
 import java.nio.FloatBuffer;
+import java.util.Objects;
 
 @Mixin(value = DSAAdvancedFboImpl.class, remap = false)
 public class LinuxFixMixin {
 
     @WrapOperation(method = "clear", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/ARBClearTexture;glClearTexImage(IIIILjava/nio/FloatBuffer;)V", ordinal = 0))
     private void redirectMethod(int textureId, int level, int format, int type, FloatBuffer data, Operation<Void> original, @Local AdvancedFboTextureAttachment texture) {
-        if (texture.getFormat() == GL30C.GL_R8UI) {
+        if (texture.getFormat() == GL30C.GL_R8UI || Objects.equals(texture.getName(), "VeilDynamicMaterial")) {
             original.call(textureId, level, GL30C.GL_RED_INTEGER, GL11C.GL_INT, data);
-            return;
-        }
-
-        if (texture.getFormat() == 0) {
             return;
         }
 
