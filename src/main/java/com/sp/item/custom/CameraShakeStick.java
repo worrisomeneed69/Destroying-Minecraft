309005d615ae01790dc45faaedd9fbd54a2ba2f0
worrisomeneed69 <worrisomeneed69@mail.bu.app>
2026-08-25 20:45:11 +0200

    isolate client camera shake code from common class

--- src/main/java/com/sp/item/custom/CameraShakeStick.java
+++ src/main/java/com/sp/item/custom/CameraShakeStick.java
@@ -1,9 +1,5 @@
 package com.sp.item.custom;
 
-import com.sp.render.camerashake.CameraShakeManager;
-import com.sp.render.camerashake.custom.CameraShakeInstance;
-import com.sp.render.camerashake.custom.SustainedCameraShakeInstance;
-import foundry.veil.api.client.util.Easing;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -20,11 +16,18 @@ public class CameraShakeStick extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(world.isClient) {
-//            SustainedCameraShakeInstance cameraShakeInstance = new SustainedCameraShakeInstance(1.0f, 100, 20, Easing.LINEAR);
-            CameraShakeInstance cameraShakeInstance = new CameraShakeInstance(1.0f, 0.0f, 100, Easing.LINEAR);
-
-            CameraShakeManager.addCameraShake(cameraShakeInstance);
+            ClientShakeHandler.onUse();
         }
         return super.use(world, user, hand);
+    }
+
+    private static class ClientShakeHandler {
+        private static void onUse() {
+            com.sp.render.camerashake.custom.CameraShakeInstance cameraShakeInstance =
+                    new com.sp.render.camerashake.custom.CameraShakeInstance(1.0f, 0.0f, 100,
+                            foundry.veil.api.client.util.Easing.LINEAR);
+
+            com.sp.render.camerashake.CameraShakeManager.addCameraShake(cameraShakeInstance);
+        }
     }
 }
