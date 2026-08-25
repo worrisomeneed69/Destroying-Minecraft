4f5597184f601cc679ed9c0b8280ee12344b2c14
worrisomeneed69 <worrisomeneed69@mail.bu.app>
2026-08-25 21:27:55 +0200

    fix: push correct clean ShaderType

--- src/main/java/com/sp/render/ShaderType.java
+++ src/main/java/com/sp/render/ShaderType.java
@@ -1,56 +1,30 @@
 package com.sp.render;
 
 import com.mojang.serialization.Codec;
-import com.sp.DestroyingMinecraftClient;
 import com.sp.destruction.DestructionType;
-import com.sp.destruction.client.ClientDestructionEvents;
-import com.sp.render.postshaders.PostShader;
-import com.sp.render.postshaders.PostShaders;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.StringIdentifiable;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public enum ShaderType implements StringIdentifiable {
-    NONE            ("none", false, false, false, false, null),
-    NUKE            ("nuke", ClientDestructionEvents.NUKE_CLIENT.getPostShader()),
-    CRACKS          ("orbital_laser", ClientDestructionEvents.CRACKS_CLIENT.getPostShader()),
-    PLANET          ("planet", ClientDestructionEvents.PLANET_CLIENT.getPostShader()),
-    SUPERNOVA       ("supernova", null),
-    BLACK_HOLE      ("black_hole", true, false, true, true,  ClientDestructionEvents.BLACK_HOLE_CLIENT.getPostShader());
+    NONE            ("none", false, false, false, false),
+    NUKE            ("nuke", true, true, true, true),
+    CRACKS          ("orbital_laser", true, true, true, true),
+    PLANET          ("planet", true, true, true, true),
+    SUPERNOVA       ("supernova", true, true, true, true),
+    BLACK_HOLE      ("black_hole", true, false, true, true);
 
     public static final Codec<ShaderType> CODEC = StringIdentifiable.createCodec(ShaderType::values);
     final String id;
-    final List<PostShader> enabledShaders;
+    public final boolean enableShadows;
+    public final boolean enableSky;
+    public final boolean enableBloom;
+    public final boolean enablePost;
 
-    ShaderType(String id, @Nullable PostShader ... postShaders) {
-        this(id, true, true, true, true, postShaders);
-    }
-
-    ShaderType(String id, boolean enableShadows, boolean enableSky, boolean enableBloom, boolean enablePost, @Nullable PostShader ... postShaders) {
+    ShaderType(String id, boolean enableShadows, boolean enableSky, boolean enableBloom, boolean enablePost) {
         this.id = id;
-
-        this.enabledShaders = new ArrayList<>();
-
-        if(enableSky) this.enabledShaders.add(ClientDestructionEvents.SUPERNOVA_CLIENT.getPostShader());
-        if(enableShadows) this.enabledShaders.add(PostShaders.SHADOW);
-
-
-        //then add whatever shader after
-        if(postShaders != null) {
-            this.enabledShaders.addAll(Arrays.stream(postShaders).toList());
-        }
-
-        //finally add bloom and post to all of it if any shader is enabled
-        if(enableBloom) this.enabledShaders.add(PostShaders.BLOOM);
-        if(enablePost) this.enabledShaders.add(PostShaders.POST);
-    }
-
-    public List<PostShader > getEnabledShaders() {
-        return this.enabledShaders;
+        this.enableShadows = enableShadows;
+        this.enableSky = enableSky;
+        this.enableBloom = enableBloom;
+        this.enablePost = enablePost;
     }
 
     @Override
@@ -75,6 +49,7 @@ public enum ShaderType implements StringIdentifiable {
             case PLANET -> ShaderType.PLANET;
             case SUPERNOVA -> ShaderType.SUPERNOVA;
             case BLACK_HOLE -> ShaderType.BLACK_HOLE;
+            default -> ShaderType.NONE;
         };
     }
 }
